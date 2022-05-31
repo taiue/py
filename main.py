e58f981f4f7de5f93eb44083a5239de5bd2c3344
taiue <taime885@gmail.com>
2022-05-31 15:57:44 -0300

    test

---
+++ main.py
@@ -0,0 +1,12 @@
+# name = "John"
+# age = "35"
+
+# print("O homem se chama " + name + " e tem " + age + " anos")
+
+# frase = "Giraffe Academy"
+# print(frase.replace("Giraffe", "Idiota"))
+
+
+print(123123)
+
+print("trest")
